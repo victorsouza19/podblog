b2df98689758dd7e81a52187a170a97d8b7b1a11
victorsouza19 <victor.cristofalo@hotmail.com>
2021-10-19 01:36:00 -0300

    articles update

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,102 @@
 const express = require("express");
+const Category = require("../categories/Category");
 const router = express.Router();
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
-  res.send("Articles route")
+router.get("/admin/articles", (req, res) => {
+  Article.findAll({
+    include: [{model: Category}] // Faz a função do INNER JOIN categories
+  },
+    {raw: true}
+    ).then(articles => {
+
+    res.render("admin/articles/index", {
+      articles: articles
+    });
+  });
 });
 
 
 router.get("/admin/articles/new", (req, res) => {
-  res.send("New article!");
+
+  Category.findAll().then(results => {
+    res.render('admin/articles/new', {
+      categories: results
+    });
+
+  })
+});
+
+router.post("/articles/save", (req, res) => {
+  const { title, body, category } = req.body
+  
+  Article.create({
+    title: title,
+    slug: slugify(title, {lower: true}),
+    body: body,
+    categoryId: category,
+  }).then(() => {
+    res.redirect('/admin/articles');
+  });
+});
+
+router.post("/articles/delete", (req, res) => {
+  const id = parseInt(req.body.id);
+
+  if(id != undefined){
+    if(!isNaN(id)){
+      Article.destroy({
+        where: {id: id}
+  
+      }).then(() => {
+        res.redirect("/admin/articles");
+  
+      })
+
+    }else{ // if not a number
+      res.redirect("/admin/articles")
+    };
+  }else{ // if null
+    res.redirect("/admin/articles")
+  }
+});
+
+router.get("/articles/edit/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if(isNaN(id)){
+    res.redirect("/admin/articles");
+  } else {
+    Article.findByPk(id).then(article => {
+      if(article != undefined){
+        res.render('admin/articles/edit', {
+          article: article
+        });
+  
+      } else {
+        res.redirect("/admin/articles");
+      }
+    }).catch(error => {
+      res.redirect("/admin/articles");
+    });  
+  }
+});
+
+router.post("/articles/update", (req, res) => {
+  const { id, title, body, category} = req.body;
+
+  Article.update({
+    title: title, 
+    body: body,
+    slug: slugify(title, {lower: true}),
+    categoryId: category}, { 
+    where: {
+      id: id
+    } 
+  }).then(() => {
+    res.redirect('/admin/articles')
+  });
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('castersblog', 'root', 'admin',{
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  timezone: "-03:00"
 });
 
 module.exports = connection;
